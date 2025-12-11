1b8b946b2fa554a4e4f60f1d60b3cd0e5b0f5440
wasted <67445572+just-wasted@users.noreply.github.com>
2025-12-11 21:20:45 +0100

    symlink helper for sysroot

---
+++ sysroot-symlinks.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+argc=$#
+
+if [[ argc -ne 1 ]]; then
+    echo "Usage: sysroot-symlinks </absolute/path/to/sysroot>"
+    exit 1
+fi
+
+sysroot=$(realpath "$1")
+if [ ! -e "$sysroot" ]; then
+    echo "Path not found."
+    exit 1
+fi
+
+ln -s -v -T "$sysroot"/usr/lib "$sysroot"/lib
+ln -s -v -T "$sysroot"/usr/lib/aarch64-linux-gnu "$sysroot"/lib64
+ln -s -v -T "$sysroot"/usr/lib/aarch64-linux-gnu "$sysroot"/usr/lib64
+ln -s -v -T "$sysroot"/usr/lib/aarch64-linux-gnu/ld-linux-aarch64.so.1 "$sysroot"/usr/lib/ld-linux-aarch64.so.1
