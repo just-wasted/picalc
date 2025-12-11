fd4bdb53aa6ddace51338d72d3274cda1b154e04
wasted <67445572+just-wasted@users.noreply.github.com>
2025-12-11 21:53:46 +0100

    make symlinks relative to make the sysroot movable without breaking symlinks

--- sysroot-symlinks.sh
+++ sysroot-symlinks.sh
@@ -15,11 +15,11 @@ if [ ! -e "$sysroot" ]; then
     exit 1
 fi
 
-ln -s -v -T "$sysroot"/usr/lib "$sysroot"/lib
+ln -s -r -v -T "$sysroot"/usr/lib "$sysroot"/lib
 
-ln -s -v -T "$sysroot"/usr/lib/aarch64-linux-gnu "$sysroot"/lib64
+ln -s -r -v -T "$sysroot"/usr/lib/aarch64-linux-gnu "$sysroot"/lib64
 
-ln -s -v -T "$sysroot"/usr/lib/aarch64-linux-gnu "$sysroot"/usr/lib64
+ln -s -r -v -T "$sysroot"/usr/lib/aarch64-linux-gnu "$sysroot"/usr/lib64
 
-ln -s -v -T "$sysroot"/usr/lib/aarch64-linux-gnu/ld-linux-aarch64.so.1 \
+ln -s -r -v -T "$sysroot"/usr/lib/aarch64-linux-gnu/ld-linux-aarch64.so.1 \
     "$sysroot"/usr/lib/ld-linux-aarch64.so.1
