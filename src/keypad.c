19784ab8fa6555cdab6b78e6ba5dbb45490a9b91
wasted <67445572+just-wasted@users.noreply.github.com>
2026-01-03 22:20:49 +0100

    added keypad specific functionality

---
+++ src/keypad.c
@@ -0,0 +1,23 @@
+#include "keypad.h"
+#include "gpio.h"
+
+// GPIO numbers for keypad rows
+const int gpio_rows[4] = {5, 6, 13, 19};
+
+// GPIO numbers for keypad columns
+const int gpio_cols[4] = {26, 16, 20, 21};
+
+void init_keypad(void)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        gpio_set_mode(gpio_rows[i], GPIO_OUTPUT);
+        gpio_set_mode(gpio_cols[i], GPIO_INPUT);
+        gpio_pud_mode(gpio_cols[i], GPIO_PULL_DOWN);
+    }
+}
+
+int get_key(void)
+{
+    return 0;
+}
