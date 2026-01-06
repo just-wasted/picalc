958a7ac1c0eb899aa262fd25c2a17d05f8d8d94c
wasted <67445572+just-wasted@users.noreply.github.com>
2026-01-06 01:48:57 +0100

    binary prefix for define macros to fix buggy comparisons

--- src/gpio.h
+++ src/gpio.h
@@ -1,19 +1,19 @@
 #pragma once
 
 // pin modes for the function select registers
-#define GPIO_INPUT 000  // GPIO Pin is an input
-#define GPIO_OUTPUT 001 // GPIO Pin is an output
-#define GPIO_ALT0 100   // GPIO Pin takes alternate function 0
-#define GPIO_ALT1 101   // GPIO Pin takes alternate function 1
-#define GPIO_ALT2 110   // GPIO Pin takes alternate function 2
-#define GPIO_ALT3 111   // GPIO Pin takes alternate function 3
-#define GPIO_ALT4 011   // GPIO Pin takes alternate function 4
-#define GPIO_ALT5 010   // GPIO Pin takes alternate function 5
+#define GPIO_INPUT 0b000  // GPIO Pin is an input
+#define GPIO_OUTPUT 0b001 // GPIO Pin is an output
+#define GPIO_ALT0 0b100   // GPIO Pin takes alternate function 0
+#define GPIO_ALT1 0b101   // GPIO Pin takes alternate function 1
+#define GPIO_ALT2 0b110   // GPIO Pin takes alternate function 2
+#define GPIO_ALT3 0b111   // GPIO Pin takes alternate function 3
+#define GPIO_ALT4 0b011   // GPIO Pin takes alternate function 4
+#define GPIO_ALT5 0b010   // GPIO Pin takes alternate function 5
 
 // pull-up / pull-down resistor states
-#define GPIO_NOPUD 00     // No resistor is selected
-#define GPIO_PULL_DOWN 01 // Pull up resistor is selected
-#define GPIO_PULL_UP 10   // Pull down resistor is selected
+#define GPIO_NOPUD 0b00     // No resistor is selected
+#define GPIO_PULL_UP 0b01   // Pull down resistor is selected
+#define GPIO_PULL_DOWN 0b10 // Pull up resistor is selected
 
 #define HIGH 1
 #define LOW 0
