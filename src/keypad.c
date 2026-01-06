5383de245543070c29cceab7d7ad8ad2ab8638b1
wasted <67445572+just-wasted@users.noreply.github.com>
2026-01-06 04:07:21 +0100

    implemented functions to read keypresses from a keypad

--- src/keypad.c
+++ src/keypad.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define DEBOUNCE_USEC 10000
+#define SLEEP_USEC 300
+
 int keypad_init(keypad_t *kp)
 {
     for (int i = 0; i < 4; i++)
@@ -11,7 +14,7 @@ int keypad_init(keypad_t *kp)
         gpio_set_mode(kp->gpio_cols[i], GPIO_INPUT);
         gpio_set_pud_mode(kp->gpio_cols[i], GPIO_PULL_DOWN);
     }
-    usleep(1000);
+    usleep(SLEEP_USEC);
 
     // check if above settings applied successfully
     int ret_val = 0;
@@ -19,20 +22,22 @@ int keypad_init(keypad_t *kp)
     {
         if (gpio_read_mode(kp->gpio_rows[i]) != GPIO_OUTPUT)
         {
-            fprintf(stderr, "Failed to set GPIO %d to OUTPUT",
+            fprintf(stderr, "keypad init: Failed to set GPIO %d to OUTPUT\n",
                     kp->gpio_rows[i]);
 
             ret_val = -1;
         }
         if (gpio_read_mode(kp->gpio_cols[i]) != GPIO_INPUT)
         {
-            fprintf(stderr, "Failed to set GPIO %d to INPUT", kp->gpio_cols[i]);
+            fprintf(stderr, "keypad init: Failed to set GPIO %d to INPUT\n",
+                    kp->gpio_cols[i]);
 
             ret_val = -1;
         }
         if (gpio_read_pud_mode(kp->gpio_cols[i]) != GPIO_PULL_DOWN)
         {
-            fprintf(stderr, "Failed to set PULL DOWN for GPIO %d",
+            fprintf(stderr,
+                    "keypad init: Failed to set PULL DOWN for GPIO %d\n",
                     kp->gpio_cols[i]);
 
             ret_val = -1;
@@ -41,7 +46,82 @@ int keypad_init(keypad_t *kp)
     return ret_val;
 }
 
-int get_key(void)
+int keypad_cleanup(keypad_t *kp)
 {
+    for (int i = 0; i < 4; i++)
+    {
+        gpio_write_level(kp->gpio_rows[i], LOW);
+        gpio_set_mode(kp->gpio_rows[i], GPIO_INPUT);
+        gpio_set_pud_mode(kp->gpio_rows[i], GPIO_PULL_DOWN);
+    }
+    usleep(SLEEP_USEC);
+
+    // check if above settings applied successfully
+    int ret_val = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        if (gpio_read_level(kp->gpio_rows[i]) != LOW)
+        {
+            fprintf(stderr,
+                    "keypad cLeanup: Failed to set GPIO %d to level LOW\n",
+                    kp->gpio_rows[i]);
+
+            ret_val = -1;
+        }
+        if (gpio_read_mode(kp->gpio_rows[i]) != GPIO_INPUT)
+        {
+            fprintf(stderr, "keypad cLeanup: Failed to set GPIO %d to INPUT\n",
+                    kp->gpio_cols[i]);
+
+            ret_val = -1;
+        }
+        if (gpio_read_pud_mode(kp->gpio_rows[i]) != GPIO_PULL_DOWN)
+        {
+            fprintf(stderr,
+                    "keypad cLeanup: Failed to set PULL DOWN for GPIO %d\n",
+                    kp->gpio_cols[i]);
+
+            ret_val = -1;
+        }
+    }
+    return ret_val;
+}
+
+char read_keypad(keypad_t *kp)
+{
+    for (int current_row = 0; current_row < 4; current_row++)
+    {
+        // set current_row HIGH, all other rows LOW
+        for (int i = 0; i < 4; i++)
+        {
+            if (i == current_row)
+            {
+                gpio_write_level(kp->gpio_rows[i], HIGH);
+            }
+            else
+            {
+                gpio_write_level(kp->gpio_rows[i], LOW);
+            }
+        }
+        usleep(SLEEP_USEC);
+
+        for (int curren_col = 0; curren_col < 4; curren_col++)
+        {
+            if (gpio_read_level(kp->gpio_cols[curren_col]) == HIGH)
+            {
+                // wait till button is released
+                do
+                {
+                    usleep(DEBOUNCE_USEC);
+                } while (gpio_read_level(kp->gpio_cols[curren_col]) == HIGH);
+
+                // debounce button release
+                usleep(DEBOUNCE_USEC);
+
+                return kp->keys[current_row][curren_col];
+            }
+            usleep(DEBOUNCE_USEC);
+        }
+    }
     return 0;
 }
--- src/keypad.h
+++ src/keypad.h
@@ -1,17 +1,34 @@
 #pragma once
 
+/*
+example key layout:
+        {'1', '2', '3', 'A'},
+        {'4', '5', '6', 'B'},
+        {'7', '8', '9', 'C'},
+        {'E', '0', 'F', 'D'}
+*/
 typedef struct
 {
     const char keys[4][4];  // list of characters assigned to each key
     const int gpio_rows[4]; // GPIO pin numbers wired to keypad rows
     const int gpio_cols[4]; // GPIO pin numbers wired to keypad columns
 } keypad_t;
-//
-// example key layout
-// 	'1', '2', '3', 'A',
-// 	'4', '5', '6', 'B',
-// 	'7', '8', '9', 'C',
-// 	'E', '0', 'F', 'D'
 
-// receives a configured keypad_t to setup GPIO, returns -1 on failure
+/*
+receives a keypad_t to setup GPIOs,
+returns -1 on failure
+*/
 int keypad_init(keypad_t *kp);
+
+/*
+receives a keypad_t to cleanup GPIOs, returns -1 on failure,
+resets GPIOs to default settings (INPUT, PULL_DOWN, level LOW)
+*/
+int keypad_cleanup(keypad_t *kp);
+
+/*
+receives a keypad_t to read pressed keys,
+returns the char associated with the pressed key
+or 0 if no key was pressed
+*/
+char read_keypad(keypad_t *kp);
